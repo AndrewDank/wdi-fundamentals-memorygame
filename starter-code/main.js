d3e71a3e1c003664286dd871f4403c2c91c85383
ANDREW DANK <andrewdank@ANDREWs-MacBook-Pro.local>
2016-11-23 21:54:10 -0800

    updated main.js

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,5 +1,5 @@
 //console.log("JS file is connected to HTML! Woo!")
-var cardOne = "queen";
+/*var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
@@ -8,6 +8,16 @@ if(cardOne === cardTwo){
 	alert('You foud a match!');
 } else {
 	alert('Sorry, try again.');
-}
+} */
 
+var board = document.getElementById('game-board');
+
+function createBoard() {
+	for (var i=0; i <cards.length; i++) {
+		var cardElement = document.createElement('div');
+
+		cardElement.className = 'card';
+		board.appendChild(cardElement);
+}
+}
 
